909f7e141a99524a5f056b112fd19efe1a9e7a34
Heitor Ferrari <heitorferrari9@gmail.com>
2026-03-22 21:01:32 -0300

    Controller

--- GerenciamentoAutores/src/main/java/com/example/GerenciamentoAutores/Controller/AutorController.java
+++ GerenciamentoAutores/src/main/java/com/example/GerenciamentoAutores/Controller/AutorController.java
@@ -1,4 +1,53 @@
 package com.example.GerenciamentoAutores.Controller;
 
+import com.example.GerenciamentoAutores.Models.AutorModel;
+import com.example.GerenciamentoAutores.Services.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping(path = "/autores")
+
 public class AutorController {
+
+    @Autowired
+    private AutorService autorService;
+
+    @PostMapping
+    public ResponseEntity<AutorModel> criarAluno(@RequestBody AutorModel produtoModel) {
+
+        AutorModel request = autorService.criarAutor(produtoModel);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(produtoModel.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorModel>> buscarAutores () {
+        List<AutorModel> request = autorService.buscarAutor();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<AutorModel>> buscarPorId(@PathVariable Long id) {
+        Optional<AutorModel> request = autorService.buscarPorId(id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?>deletarProduto (@PathVariable Long id) {
+        autorService.removerAutor(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- GerenciamentoAutores/src/main/java/com/example/GerenciamentoAutores/Services/AutorService.java
+++ GerenciamentoAutores/src/main/java/com/example/GerenciamentoAutores/Services/AutorService.java
@@ -2,21 +2,32 @@ package com.example.GerenciamentoAutores.Services;
 
 import com.example.GerenciamentoAutores.Models.AutorModel;
 import com.example.GerenciamentoAutores.Repositorie.AutorRepositorie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AutorService {
 
+    @Autowired
     private AutorRepositorie autorRepositorie;
 
     public AutorModel criarAutor(AutorModel autorModel) {
         return autorRepositorie.save(autorModel);
     }
 
-    public List<AutorModel> buscarAutor (AutorModel autorModel) {
+    public List<AutorModel> buscarAutor () {
         return autorRepositorie.findAll();
+    }
+
+    public void removerAutor (Long id) {
+        autorRepositorie.deleteById(id);
+    }
+
+    public Optional<AutorModel> buscarPorId (Long id) {
+        return autorRepositorie.findById(id);
     }
 
 }
